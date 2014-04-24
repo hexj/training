1c164e543c109fd16464d62b4c939a69b1de5991
Jonathan.X.J.He <hexiaojiao@gmail.com>
2014-04-24 23:43:47 +0800

    Create UserMapper.java

---
+++ practice/ibatis-app/src/main/java/forwe/hexj/learn/mybatis/dao/UserMapper.java
@@ -0,0 +1,17 @@
+package forwe.hexj.learn.mybatis.dao;
+
+import forwe.hexj.learn.mybatis.model.User;
+
+public interface UserMapper {
+    int deleteByPrimaryKey(Integer userid);
+
+    int insert(User record);
+
+    int insertSelective(User record);
+
+    User selectByPrimaryKey(Integer userid);
+
+    int updateByPrimaryKeySelective(User record);
+
+    int updateByPrimaryKey(User record);
+}
